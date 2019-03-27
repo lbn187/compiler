da78bcca9b57dcea2d66153bb5f1846785797c9f
lbn187 <lbn187@sina.com>
2019-03-27 09:38:22 +0800

    190327 - addlib

--- semantic.bash
+++ semantic.bash
@@ -1,11 +1,9 @@
-#!/bin/bash
-# this script is called when the judge wants our compiler to compile a source file.
+# this script is called when the judge wants your compiler to compile a source file.
 # print the compiled source, i.e. asm code, directly to stdout.
-# don't print anything other to stdout.
+# don't print anything else to stdout.
 # if you would like to print some debug information, please go to stderr.
-# $1 is the path to the source file.
-# $2 is the path to the target file.
 
 set -e
 cd "$(dirname "$0")"
-java -cp ./lib/*:./bin Main
+export CCHK="java -classpath ./lib/antlr-4.7.1-complete.jar:./bin src.Main"
+$CCHK
